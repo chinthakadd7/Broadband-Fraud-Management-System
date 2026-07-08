97ed120d11241f70deefed4313e5e2c3597bd325
Sarasi <eg245387@engug.ruh.ac.lk>
2026-07-09 01:47:41 +0530

    test

--- scripts/sample2.py
+++ scripts/sample2.py
@@ -17,22 +17,22 @@ collection = db["transactions"]
 sample_records = [
     # --- Obvious fraud patterns ---
     {
-        "customer_id": "CUST-TEST-F001",
-        "usage_mb": 18500.0,
-        "avg_usage_mb": 1200.0,
+        "customer_id": "CUST-TEST-F0011",
+        "usage_mb": 1850.0,
+        "avg_usage_mb": 120.0,
         "device_age_days": 0,
-        "num_devices_30d": 15,
+        "num_devices_30d": 145,
         "failed_payments_7d": 4,
-        "account_age_days": 2,
+        "account_age_days": 12,
         "login_hour": 3,
         "distance_from_usual_km": 220.0,
         "mac_address": "FF:FF:FF:11:22:33",
     },
     {
-        "customer_id": "CUST-TEST-F002",
-        "usage_mb": 22000.0,
+        "customer_id": "CUST-TEST-F0021",
+        "usage_mb": 2200.0,
         "avg_usage_mb": 1500.0,
-        "device_age_days": 1,
+        "device_age_days": 121,
         "num_devices_30d": 12,
         "failed_payments_7d": 3,
         "account_age_days": 1,
@@ -42,10 +42,10 @@ sample_records = [
     },
     {
         # Hard-block: blacklisted MAC from your rules.yaml
-        "customer_id": "CUST-TEST-F003",
+        "customer_id": "CUST-TEST-F013",
         "usage_mb": 5000.0,
         "avg_usage_mb": 2000.0,
-        "device_age_days": 10,
+        "device_age_days": 1140,
         "num_devices_30d": 4,
         "failed_payments_7d": 0,
         "account_age_days": 100,
@@ -56,22 +56,22 @@ sample_records = [
 
     # --- Obvious legitimate patterns ---
     {
-        "customer_id": "CUST-TEST-L001",
+        "customer_id": "CUST-TEST-L0014",
         "usage_mb": 1400.0,
         "avg_usage_mb": 1450.0,
         "device_age_days": 400,
         "num_devices_30d": 4,
-        "failed_payments_7d": 0,
+        "failed_payments_7d": 10,
         "account_age_days": 600,
         "login_hour": 19,
         "distance_from_usual_km": 2.0,
         "mac_address": "AA:BB:CC:10:20:30",
     },
     {
-        "customer_id": "CUST-TEST-L002",
+        "customer_id": "CUST-TEST-L0024",
         "usage_mb": 1800.0,
         "avg_usage_mb": 1700.0,
-        "device_age_days": 250,
+        "device_age_days": 2150,
         "num_devices_30d": 5,
         "failed_payments_7d": 0,
         "account_age_days": 900,
@@ -82,11 +82,11 @@ sample_records = [
 
     # --- Borderline / REVIEW-ish pattern ---
     {
-        "customer_id": "CUST-TEST-R001",
+        "customer_id": "CUST-TEST-R0015",
         "usage_mb": 6000.0,
         "avg_usage_mb": 2000.0,
         "device_age_days": 15,
-        "num_devices_30d": 7,
+        "num_devices_30d": 427,
         "failed_payments_7d": 1,
         "account_age_days": 20,
         "login_hour": 4,
@@ -95,10 +95,10 @@ sample_records = [
     },
 
     {
-        "customer_id": "CUST-TEST-R002",
+        "customer_id": "CUST-TEST-R0024",
         "usage_mb": 6000.0,
         "avg_usage_mb": 2000.0,
-        "device_age_days": 5,
+        "device_age_days": 15,
         "num_devices_30d": 17,
         "failed_payments_7d": 1,
         "account_age_days": 2,
